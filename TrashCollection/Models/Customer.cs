fc6c9619b1888ef62f2307c5351a713ed6e5aaad
unknown <meekhunsutp@gmail.com>
2020-11-17 11:07:22 -0600

    Created Customer Class, Address Class, ManageService Class

--- TrashCollection/Models/Customer.cs
+++ TrashCollection/Models/Customer.cs
@@ -12,10 +12,19 @@ namespace TrashCollection.Models
     {
         [Key]
         public int Id { get; set; }
-        public string Name { get; set; }
 
         [ForeignKey("IdentityUser")]
         public string IdentityUserId { get; set; }
         public IdentityUser IdentityUser { get; set; }
+
+        [ForeignKey("Address")]
+        public int AddressId { get; set; }
+        public Address Address { get; set; }
+
+
+        public string Email { get; set; }
+        public string FirstName  { get; set; }
+        public string LastName { get; set; }
+
     }
 }
